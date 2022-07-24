808e3c642643957841d044ecbd1650447bb98363
Ivan <zhanbrsv98@icloud.com>
2022-07-24 12:06:09 +0900

    0.1

---
+++ test.py
@@ -0,0 +1,120 @@
+#age = int(input("Your age?"))
+#if (age >= 25):
+#    print("welcome")
+#elif (age>=18) and (age < 25):
+#    print("with mom")
+#else:
+#    print("dont!")
+##func elif
+
+
+#def hello():
+#    print('Hello, World!')
+
+#hello()
+##hello cherez func
+
+
+#def sum(a,b):
+ #   return a + b
+#x = int(input('choose 1 number'))
+#y = int(input('choose 2 number'))
+
+#z = sum(x,y)
+#print (z)
+##summa x and y
+
+
+#name = "Hello, World!"
+#for i in range(1,11):
+#    print(name)
+##10 strok
+
+
+#print('Did Joffrey agree?')
+#print('He did. He also said\"I love using \\n".')
+##perenos strok
+
+
+#rubles_per_dollar = 60
+#dollar_count = 50 * 1.25
+#rubles_count = dollar_count * rubles_per_dollar
+
+#print('The price is ' + str(rubles_count) + ' rubles')
+##output perem and text
+
+#dollars_per_euro = 1.25
+#rubles_per_dollar = 60
+
+#euros_count = 1000
+#dollars_count = euros_count * dollars_per_euro
+#rubles_count = dollars_count * rubles_per_dollar
+
+#print(rubles_count)
+##who i am
+
+
+#stark = 'Arya'
+#print(f"Do you want to eat, {stark}?\nYes, I'm hungry, mom.")
+
+
+#one = 'Naharis'
+#two = 'Mormont'
+#three = 'Sand'
+#print(f'{one[2]}\n{two[1]}\n{three[3]}\n{two[4]}\n{two[2]}')
+##index
+
+
+#value = str(2)
+#value1 = ' times'
+#res = value + value1
+#print(res)
+##konkanetaciya
+
+
+#value = '-42'
+#value = int(-42)
+#value = abs(value)
+#print(value)
+##abs func
+
+
+#number = 10.1234
+#number = int(number)
+#number = hex(number)
+#print(number)
+##16-rich
+
+
+#from random import randint
+#print(randint(0, 11))
+
+
+#company = "hexlet"
+#print(company.capitalize())
+
+
+#first_name = '    Grigor    \n'
+#print(first_name.strip())
+
+
+#text = 'msdoasdohfmoism\n'
+#print(len(text[5:10].strip()))
+##multi
+
+
+#def show_greeting():
+#    text = 'Hello, Hexlet'
+#    print(text)
+
+#show_greeting()
+##def func
+
+
+#def multiply(num1, num2):
+#    num1 * num2
+
+result = multiply(2, 2)
+print(result)
+
+

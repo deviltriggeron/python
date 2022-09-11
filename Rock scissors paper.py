f9a5bfbde62fa9eeab6ef5fad2bac5131710fe2e
Ivan <zhanbrsv98@icloud.com>
2022-09-11 14:31:09 +0900

    try win

--- Rock scissors paper.py
+++ Rock scissors paper.py
@@ -1,9 +1,18 @@
-rsp = input('choose number "rock = 1" "scissors = 2", "paper = 3"\n')
-if rsp=="1" :
-    print("you choose rock \ni'm choose paper, you lose")
-if rsp=="2" :
-    print("you choose scissors \ni'm choose rock, you lose")
-if rsp=="3" :
-    print ("you choose paper \ni'm choose scissors, you lose")
-else :
-    print('choose again')
+game = 'rock.scissors.paper'
+
+while True:
+    rsp = input('choose number "rock = 1" "scissors = 2", "paper = 3"\n')
+    if rsp == "1":
+        print("you choose rock \ni'm choose paper, you lose \ntry again? or if you are scared then write 'N'")
+    if rsp == "2":
+        print("you choose scissors \ni'm choose rock, you lose \ntry again? or if you are scared then write 'N' ")
+    if rsp == "3":
+        print("you choose paper \ni'm choose scissors, you lose \ntry again? or if you are scared then write 'N'")
+    if rsp == "N":
+        exit(game)
+    if rsp == "n":
+        exit(game)
+    else:
+        print("choose again, think\n")
+
+
